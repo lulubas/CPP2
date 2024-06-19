97d388d2721a011562004ef00deeef3a72109fbb
Louis B <lb@gmail.com>
2024-06-19 14:51:23 +0200

    ex02 finished

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,16 +6,39 @@
 /*   By: lbastien <lbastien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 12:58:59 by lbastien          #+#    #+#             */
-/*   Updated: 2024/06/18 16:38:01 by lbastien         ###   ########.fr       */
+/*   Updated: 2024/06/19 14:51:05 by lbastien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"Fixed.hpp"
 
-Fixed::Fixed() : _fixedPointValue(0) {}
+Fixed::Fixed() : _fixedPointValue(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
 
 Fixed::Fixed(const Fixed &other) {
+    std::cout << "Copy constructor called" << std::endl;
     _fixedPointValue = other._fixedPointValue;
 }
 
+Fixed &Fixed::operator=(const Fixed &other) {
+    std::cout << "Copy assignement constructor called" << std::endl;
+    if (this != &other) {
+        _fixedPointValue = other._fixedPointValue;        
+    }
+    return *this;
+}
 
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) {
+    std::cout << "getRawBits function called" << std::endl;
+    return _fixedPointValue;
+}
+
+void Fixed::setRawbits( int const raw ) {
+    std::cout << "setRawBits function called" << std::endl;
+    _fixedPointValue = raw;
+}
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,19 +6,24 @@
 /*   By: lbastien <lbastien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 13:11:07 by lbastien          #+#    #+#             */
-/*   Updated: 2024/06/18 16:35:15 by lbastien         ###   ########.fr       */
+/*   Updated: 2024/06/19 14:41:19 by lbastien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed {
     public:
         Fixed();
         Fixed(const Fixed &other);
-
+        Fixed &operator=(const Fixed &other);
         ~Fixed();
+
+        int     getRawBits( void );
+        void    setRawbits( int const raw );
 
     private:
         int _fixedPointValue;
